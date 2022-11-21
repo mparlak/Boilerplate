ee3541e78de6566771c912ade6a533f3ec97ef4c
Muhammet Parlak <muhammetpparlak@gmail.com>
2022-11-21 14:02:57 +0300

    Update IRepository.cs

--- src/core/Boilerplate.Application/Common/Persistence/IRepository.cs
+++ src/core/Boilerplate.Application/Common/Persistence/IRepository.cs
@@ -2,94 +2,99 @@ namespace Boilerplate.Application.Common.Persistence;
 
 public interface IRepository<TEntity>
 {
-    #region Methods
-
     /// <summary>
-    /// Get entity by identifier
-    /// </summary>
-    /// <param name="id">Identifier</param>
-    /// <returns>Entity</returns>
-    TEntity GetById(object id);
+        /// Get entity by identifier
+        /// </summary>
+        /// <param name="id">Identifier</param>
+        /// <returns>Entity</returns>
+        TEntity? GetById(object id);
+        /// <summary>
+        /// Get entity by identifier
+        /// </summary>
+        /// <param name="id">Identifier</param>
+        /// <returns>Entity</returns>
+        Task<TEntity?> GetByIdAsync(object id);
+        /// <summary>
+        /// Insert entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        void Insert(TEntity entity);
 
-    /// <summary>
-    /// Get entity by identifier
-    /// </summary>
-    /// <param name="id">Identifier</param>
-    /// <returns>Entity</returns>
-    Task<TEntity> GetByIdAsync(object id);
+        /// <summary>
+        /// Insert entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        Task InsertAsync(TEntity entity);
+        /// <summary>
+        /// Insert entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        void BulkInsert(IEnumerable<TEntity> entities);
 
-    /// <summary>
-    /// Insert entity
-    /// </summary>
-    /// <param name="entity">Entity</param>
-    void Insert(TEntity entity);
 
-    /// <summary>
-    /// Insert entity
-    /// </summary>
-    /// <param name="entity">Entity</param>
-    Task InsertAsync(TEntity entity);
+        /// <summary>
+        /// Insert entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        Task BulkInsertAsync(IEnumerable<TEntity> entities);
 
-    /// <summary>
-    /// Insert entities
-    /// </summary>
-    /// <param name="entities">Entities</param>
-    void Insert(IEnumerable<TEntity> entities);
+        /// <summary>
+        /// Update entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        void Update(TEntity entity);
+        /// <summary>
+        /// Update entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        Task UpdateAsync(TEntity entity);
+        /// <summary>
+        /// Update entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        void BulkUpdate(IEnumerable<TEntity> entities);
 
-    /// <summary>
-    /// Update entity
-    /// </summary>
-    /// <param name="entity">Entity</param>
-    void Update(TEntity entity);
+        /// <summary>
+        /// Update entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        Task BulkUpdateAsync(IEnumerable<TEntity> entities);
 
-    /// <summary>
-    /// Update entity
-    /// </summary>
-    /// <param name="entity">Entity</param>
-    Task UpdateAsync(TEntity entity);
+        /// <summary>
+        /// Delete entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        void Delete(TEntity entity);
 
-    /// <summary>
-    /// Update entities
-    /// </summary>
-    /// <param name="entities">Entities</param>
-    void Update(IEnumerable<TEntity> entities);
+        /// <summary>
+        /// Delete entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        Task DeleteAsync(TEntity entity);
 
-    /// <summary>
-    /// Delete entity
-    /// </summary>
-    /// <param name="entity">Entity</param>
-    void Delete(TEntity entity);
+        /// <summary>
+        /// Delete entities
+        /// </summary>
+        /// <param name="entities">Entities</param>
+        void BulkDelete(IEnumerable<TEntity> entities);
 
-    /// <summary>
-    /// Delete entity
-    /// </summary>
-    /// <param name="entity">Entity</param>
-    Task DeleteAsync(TEntity entity);
+        /// <summary>
+        /// Gets a table
+        /// </summary>
+        IQueryable<TEntity> GetAll();
 
-    /// <summary>
-    /// Delete entities
-    /// </summary>
-    /// <param name="entities">Entities</param>
-    void Delete(IEnumerable<TEntity> entities);
+        /// <summary>
+        /// Gets a table with "no tracking" enabled (EF feature) Use it only when you load record(s) only for read-only operations
+        /// </summary>
+        IQueryable<TEntity> GetAllAsNoTracking();
 
-    #endregion
+        /// <summary>
+        /// Gets a table with paged response
+        /// </summary>
+        IQueryable<TEntity> GetAllWithPagination(int pageNumber, int pageSize);
 
-    #region Properties
-
-    /// <summary>
-    /// Gets a table
-    /// </summary>
-    IQueryable<TEntity> GetAll();
-
-    /// <summary>
-    /// Gets a table with "no tracking" enabled (EF feature) Use it only when you load record(s) only for read-only operations
-    /// </summary>
-    IQueryable<TEntity> GetAllAsNoTracking();
-
-    /// <summary>
-    /// Gets a table with paged response
-    /// </summary>
-    IQueryable<TEntity> GetAll(int pageNumber, int pageSize);
-
-    #endregion
+        /// <summary>
+        ///save context changes
+        /// </summary>
+        Task SaveAsync();
 }
